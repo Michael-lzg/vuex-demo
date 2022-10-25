129f85622e7c0df92ae2af1c1fdb360dda490e9a
lzg <19802021763@139.com>
2022-10-25 10:51:11 +0800

    验证一下

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
+    <div id="nav">
+      <router-link to="/">Home12312</router-link> |
+      <router-link to="/about">About222</router-link>
+    </div>
     <router-view />
   </div>
 </template>
